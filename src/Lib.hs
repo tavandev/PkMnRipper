ae2837e86e3c24b1f878acfd377fe7aca307817b
Dimitri TAVAN <mimi@mbp-de-dimitri.home>
2018-01-30 01:17:40 +0100

    Travis bis 	modified:   .travis.yml 	modified:   src/Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,8 +17,8 @@ import           Data.Function
 import           Data.List
 import           Data.Monoid
 import           Data.Word
-import           Utils
 import           Numeric
+import           Utils
 
 data Pokemon = Pokemon
   { index      :: Word8
@@ -36,27 +36,29 @@ data Pokemon = Pokemon
 instance ToJSON Pokemon where
   toJSON Pokemon {..} =
     object
-      [ "index"   .= index
-      , "name"    .= name
-      , "hp"      .= hp
-      , "attack"  .= attack
+      [ "index" .= index
+      , "name" .= name
+      , "hp" .= hp
+      , "attack" .= attack
       , "defense" .= defense
-      , "speed"   .= speed
+      , "speed" .= speed
       , "special" .= special
-      , "type1"   .= type1
-      , "type2"   .= type2 ]
-
+      , "type1" .= type1
+      , "type2" .= type2
+      ]
   toEncoding Pokemon {..} =
     pairs $
-        "index"   .= index 
-     <> "name"    .= name 
-     <> "hp"      .= hp 
-     <> "attack"  .= attack 
-     <> "defense" .= defense 
-     <> "speed"   .= speed 
-     <> "special" .= special 
-     <> "type1"   .= type1 
-     <> "type2"   .= type2
+    "index" .= index <> "name" .= name <> "hp" .= hp <> "attack" .= attack <>
+    "defense" .=
+    defense <>
+    "speed" .=
+    speed <>
+    "special" .=
+    special <>
+    "type1" .=
+    type1 <>
+    "type2" .=
+    type2
 
 indexOrder :: B.ByteString -> [Word8]
 indexOrder = slice 0x41036 190 >>> B.unpack
@@ -69,36 +71,25 @@ types s = toTypeArray s <$> typePointers s
   where
     typePointers =
       slice 0x27D63 0x36 >>> B.unpack >>> chunks 2 >>> fmap processP
-    
     processP (low:high:_) =
-      shiftL 2 16 .|. shiftL (fromIntegral high) 8 .|. fromIntegral low 
-    
+      shiftL 2 16 .|. shiftL (fromIntegral high) 8 .|. fromIntegral low
     toTypeArray source offset =
-      B.splitAt offset source 
-          & \(_, d) -> B.takeWhile (/= 0x50) d 
-          & B.unpack 
-          & fmap toChar 
-          & capitalized
+      B.splitAt offset source & \(_, d) ->
+        B.takeWhile (/= 0x50) d & B.unpack & fmap toChar & capitalized
 
 toPokemons :: B.ByteString -> [Pokemon]
 toPokemons s = toPkmn s <$> zip (sortedPokemon s) (pokemonBaseStats s)
   where
-    doubleType a b  
+    doubleType a b
       | a == b = (a, Nothing)
       | otherwise = (a, Just b)
-
     pokemonNames src =
-      slice 0xE8000 1900 src
-          & B.unpack 
-          & fmap toChar 
-          & chunks 10 
-          & fmap (filter (/= ' ') >>> capitalized)
-    
+      slice 0xE8000 1900 src & B.unpack & fmap toChar & chunks 10 &
+      fmap (filter (/= ' ') >>> capitalized)
     sortedPokemon src =
-      zip3 (indexOrder src) [1 ..] (pokemonNames src) 
-          & filter (\(idx, _, _) -> idx /= 0) 
-          & sort
-    
+      zip3 (indexOrder src) [1 ..] (pokemonNames src) &
+      filter (\(idx, _, _) -> idx /= 0) &
+      sort
     toPkmn src ((idx, int, name), b) =
       let (t1, t2) =
             doubleType
@@ -106,18 +97,31 @@ toPokemons s = toPkmn s <$> zip (sortedPokemon s) (pokemonBaseStats s)
               (fromIntegral (b !! 7) & (types src !!))
       in Pokemon idx int name (b !! 1) (b !! 2) (b !! 3) (b !! 4) (b !! 5) t1 t2
 
-spriteOffset :: (Bits a, Num a) => Int -> B.ByteString -> a
-spriteOffset idx s = shiftL (getBank idx) 14 + ptr idx s .&. 0x3FFF
-    where 
-    getBank a 
-        | a < 0x1F  = 0x9
-        | a < 0x4A  = 0xA
-        | a < 0x74  = 0xB
-        | a < 0x99  = 0xC
+data SpriteData = SpriteData
+  { ptr    :: Word32
+  , height :: Word8
+  , width  :: Word8
+  } deriving (Show)
+
+spriteOffset :: Int -> B.ByteString -> SpriteData
+spriteOffset idx s =
+  let (w, h) = size idx s
+  in SpriteData (shiftL (getBank idx) 14 + ptr idx s .&. 0x3FFF) w h
+  where
+    getBank a
+        | a < 0x1F = 0x9
+        | a < 0x4A = 0xA
+        | a < 0x74 = 0xB
+        | a < 0x99 = 0xC
         | otherwise = 0xD
-        
-    ptr n src = 
-      pokemonBaseStats src & (!! n) & ((!! 0xB) &&& (!! 0xC)) & process
-    
-    process (low, high) = 
-          shiftL (fromIntegral high) 8 .|. fromIntegral low
+
+    ptr n src =
+      pokemonBaseStats src 
+          & (!! n) 
+          & ((!! 0xB) &&& (!! 0xC)) 
+          & (\(low, high) -> shiftL (fromIntegral high) 8 .|. fromIntegral low)
+    size n src =
+      pokemonBaseStats src & (!! n) & (!! 0xA) &
+      (\b -> (shiftR (fromIntegral b) 4 * 8, fromIntegral b .&. 0xF * 8))
+
+hex n = "0x" ++ showHex n ""
